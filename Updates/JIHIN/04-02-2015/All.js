15c0b71a36f30681c0f13d4711696e5bd6363c3d
Jihin <jihin@baabte.com>
2015-02-09 19:30:21 +0530

    EditCouresElement,Remove Course Element by Jihin

--- Updates/JIHIN/04-02-2015/All.js
+++ Updates/JIHIN/04-02-2015/All.js
@@ -1,37 +1,4 @@
+db.system.js.save({_id:"fnEditCourseElement",value:function(g,a,f,d,c){var b="courseTimeline."+f+"."+a;var e={};e[b]=d;db.clnCourses.update({_id:g},{$set:e});return db.clnCourses.find({_id:g}).toArray()}});
 
-
-db.system.js.save({_id: "fnEditCourseElement",
-		value: function(courseId, courseElemName, tlPoint, index, rmId) {
-		var key = "courseTimeline."+tlPoint+"."+courseElemName;
-		var obj = {};
-		obj[key] = index;
-		db.clnCourses.update({_id:courseId},{$set:obj});
-		}});
-
-
-
-
-db.system.js.save({_id: "fnRemoveCourseElement",
-		value: function(courseId, courseElemName, tlPoint, index, rmId) {
-		var key="courseTimeline."+tlPoint+"."+courseElemName;
-		var obj={};
-		obj[key]=index;
-		db.clnCourses.update({_id:courseId},{$pop:obj});
-		var a =db.clnCourses.findOne({_id:courseId});
-		if(!a.courseTimeline[tlPoint][courseElemName].length){
-		b = a.courseTimeline[tlPoint][courseElemName];
-		var unset={};
-		unset[key]=1;
-		db.clnCourses.update({_id:courseId},{$unset:unset});
-		a = db.clnCourses.findOne({_id:courseId});
-		if(Object.keys(a.courseTimeline[tlPoint]).length == 0){
-			unset={};
-			unset["courseTimeline."+tlPoint]=1;
-			db.clnCourses.update({_id:courseId},{$unset:unset});
-			b=unset;
-			}
-		}
-		return "courseTimeline."+tlPoint;
-	}});
-
+db.system.js.save({_id:"fnRemoveCourseElement",value:function(f,d,h,g,c){var j="courseTimeline."+h+"."+d;var e={};e[j]=g;db.clnCourses.update({_id:f},{$pop:e});var i=db.clnCourses.findOne({_id:f});if(!i.courseTimeline[h][d].length){b=i.courseTimeline[h][d];var k={};k[j]=1;db.clnCourses.update({_id:f},{$unset:k});i=db.clnCourses.findOne({_id:f});if(Object.keys(i.courseTimeline[h]).length==0){k={};k["courseTimeline."+h]=1;db.clnCourses.update({_id:f},{$unset:k});b=k}}return"courseTimeline."+h}});
 
