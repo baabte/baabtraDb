e755525ec589f6f38cf3fe7be8f8a963f8909d9d
Arun R Menon <arun@baabte.com>
2015-02-25 19:00:25 +0530

    commit all changes

--- Updates/Arun/25-02-14.js
+++ Updates/Arun/25-02-14.js
@@ -165,3 +165,14 @@ var  userId = db.clnUserLogin.findOne({userName:data.eMail},{_id:1});
     
     return result;
 }});
+
+db.system.js.save({_id: "fnFetchSpecificCustomForm",
+      value: function (FetchSpecificFormObj){
+        var resultmsg;
+        
+      var roleSchema=db.clnUserCustomForms.findOne({fkcompanyId:ObjectId(FetchSpecificFormObj.companyId),formName:FetchSpecificFormObj.fetchFormName},{_id:0,roleSchema:1});
+  resultmsg='specific form fetch company for rolebased forms';
+var result={'result':resultmsg,roleSchema:roleSchema}
+    return result;
+}
+}); 
