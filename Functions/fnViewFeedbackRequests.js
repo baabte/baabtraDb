4f39103fc7312e62e1b36c565bffa18e41600672
Jihin <jihin@baabte.com>
2015-03-12 21:41:53 +0530

    12-03-2015 by Jihin

--- Functions/fnViewFeedbackRequests.js
+++ Functions/fnViewFeedbackRequests.js
@@ -1,10 +1,15 @@
 /*
 Created by : Jihin
 Created On : 11/03/2015
+Updated On : 12/03/2015
 Purpose : View Feedback Requests
 */
 db.system.js.save({_id: "fnViewFeedbackRequests",
         value:function(rmId, companyId) {
-    var FeedbackRequests = db.clnFeedbacks.find({targetList:{$in:[ObjectId(rmId)]},companyId:ObjectId(companyId)},{title:1,description:1}).toArray();
-    return FeedbackRequests;
+    var feedbackRequests = {};
+    var pendingFeedback = db.clnFeedbacks.find({targetList:{$in:[ObjectId(rmId)]},companyId:ObjectId(companyId),userResponse:{$not:{$elemMatch:{rmId:ObjectId(rmId)}}},validUntil:{$gt:ISODate()}},{title:1,description:1,createdDate:1}).sort( {createdDate: -1}).toArray();
+    var submitedFeedback = db.clnFeedbacks.find({targetList:{$in:[ObjectId(rmId)]},companyId:ObjectId(companyId),userResponse:{$elemMatch:{rmId:ObjectId(rmId)}}},{title:1,description:1,createdDate:1}).sort( {createdDate: -1}).toArray();
+    feedbackRequests.pendingFeedback = pendingFeedback;
+    feedbackRequests.submitedFeedback = submitedFeedback;
+    return feedbackRequests;
 }});
