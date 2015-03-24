14a7660fb81c5de9fc0eee6b12cce9e0f96a5367
midhun123 <midhun@baabte.com>
2015-03-24 20:37:19 +0530

    globalexisting conf

--- Updates/midhun/24-03-2014/24-03-2015.js
+++ Updates/midhun/24-03-2014/24-03-2015.js
@@ -6,3 +6,31 @@ db.system.js.save({_id: "fnretrieveExistingGlobalConf",
     return returnData;
 }
 });
+
+
+db.system.js.save({_id: "fnUpdateUserProfileDatas",
+		value:function (data) {
+    if (data.userDetailsObjId) {
+        response = db.clnUserDetails.update({_id:ObjectId(data.userDetailsObjId)}, {$set:{profile:data.profile}});
+        if (data.profile.Preferedlanguage) {
+            db.clnActiveUserData.update({userLoginId:data.userLoginId}, {$set:{Preferedlanguage:data.profile.Preferedlanguage}});
+        }
+    } else {
+        var newCollection = {};
+        newCollection.crmId = ObjectId(data.userLoginId);
+        newCollection.urmId = ObjectId(data.userLoginId);
+        newCollection.fkUserLoginId = ObjectId(data.userLoginId);
+        newCollection.createdDate = Date();
+        newCollection.updatedDate = Date();
+        newCollection.activeFlag = 1;
+        newCollection.profile = data.profile;
+        db.clnUserDetails.save(newCollection);
+        if (data.profile.Preferedlanguage) {
+            db.clnActiveUserData.update({userLoginId:data.userLoginId}, {$set:{Preferedlanguage:data.profile.Preferedlanguage}});
+        }
+        return "success";
+    }
+}
+});
+
+
