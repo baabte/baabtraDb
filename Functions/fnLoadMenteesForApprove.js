40baad83de2295eecbf204a017bbf81cc97856b4
jihin <jihin@baabte.com>
2015-03-25 18:06:33 +0530

    LoadMenteesForApprove by Jihin

--- Functions/fnLoadMenteesForApprove.js
+++ Functions/fnLoadMenteesForApprove.js
@@ -7,17 +7,17 @@ purpose : For Load Mentees For Approve
 
 db.system.js.save({_id:'fnLoadMenteesForApprove',
 value:function(companyId, statusType) {
-    /*var userLogins = db.clnUserRoleMapping.distinct('fkUserLoginId',{profile:{$elemMatch:{fkCompanyId:ObjectId(companyId)}},fkRoleId:3})
-    var userDetails = db.clnUserDetails.find({status:statusType,fkUserLoginId:{$in:userLogins}},{fkUserLoginId:1, profile:1, status:1,crmId:1})
-    var userObject = {};
-    var index = 0;
-    userDetails.forEach(function(user){
-        var userInfo = db.clnUserLogin.findOne({_id:user.fkUserLoginId},{userName:1}); 
-        user.email = userInfo.userName;
-        userObject[index] = {};
-        userObject[index] =  user;
-        index = index + 1;
-        });*/
+    var data = {};
+    data.orderFroms = db.clnTrainingRequest.find({companyId:ObjectId(companyId)},{orderDetails:1,orderFormId:1,createdDate:1,crmId:1}).toArray();
+    var company = db.clnCompany.findOne({_id:ObjectId(companyId)});
+    var companyDetails ={};
+    companyDetails.companyName = company.companyName;
+    companyDetails.Address = company.Address;
+    companyDetails.Phone = company.Phone;
+    companyDetails.eMail = company.eMail;
+    companyDetails.Fax = company.Fax;
+    companyDetails.companyLogo = company.appSettings.logo;
+    data.companyDetails = companyDetails;
     
-	return db.clnTrainingRequest.find({companyId:ObjectId(companyId)},{orderDetails:1,orderFormId:1,crmId:1}).toArray();
+return data;
 }});
