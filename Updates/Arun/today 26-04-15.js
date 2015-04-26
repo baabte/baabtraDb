7842405c883be268bb942c504b29c10fa7f54350
Arun R Menon <arun@baabte.com>
2015-04-26 21:47:09 +0530

    commit with latest fuction changes

--- Updates/Arun/today 26-04-15.js
+++ Updates/Arun/today 26-04-15.js
@@ -65,3 +65,74 @@ db.system.js.save({
 db.clnUserCourseMapping.save(course);
        return {success:true};
 }});
+
+
+
+//fnSubmitTest
+db.system.js.save({
+    "_id" : "fnSubmitTest",
+    "value" : function (SubmitTestObj) {
+          var courseMappingId=ObjectId(SubmitTestObj.courseMappingId);
+          var userLoginId=ObjectId(SubmitTestObj.userLoginId);
+          var keyName=SubmitTestObj.keyName;
+          var tlPointInmins=SubmitTestObj.tlPointInmins;
+          var outerIndex=SubmitTestObj.outerIndex;
+          var innerIndex=SubmitTestObj.innerIndex;
+          var timeObj=SubmitTestObj.timeObj;
+          var userAnswers=SubmitTestObj.userAnswers;
+          var totalMarkScored=SubmitTestObj.totalMarkScored;
+          var uniquekey;
+          var resultmsg;
+          var targetList=[];
+
+          var evalStatus='';
+          for(var index in userAnswers){
+
+            if(userAnswers[index].evaluated==0){
+            evalStatus='Pending Evaluation';
+            }
+          }
+
+          if(evalStatus==''){
+            evalStatus='Evaluated';
+            }
+
+var course=db.clnUserCourseMapping.findOne({_id:courseMappingId,activeFlag:1});  
+uniquekey=course._id.valueOf()+'.'+tlPointInmins+'.'+keyName+'.'+outerIndex;
+course.courseTimeline[tlPointInmins][keyName][outerIndex].evalStatus=evalStatus;
+//checks if he have already scored marks
+    if(!course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored){
+    course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored=0;  
+    }
+   
+    course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored=course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored+totalMarkScored;
+    //checks if he have already scored marks
+    if(!course.courseTimeline[tlPointInmins].markScored){
+    course.courseTimeline[tlPointInmins].markScored=0;  
+    }
+   
+    course.courseTimeline[tlPointInmins].markScored=course.courseTimeline[tlPointInmins].markScored+totalMarkScored;
+   
+    //checks if he have already scored marks
+    if(!course.markScored){
+    course.markScored=0;  
+    }  
+   
+    course.markScored=course.markScored+totalMarkScored;
+    
+     course.courseTimeline[tlPointInmins][keyName][outerIndex][timeObj.key]=timeObj.value;  
+   
+
+    for(var index in userAnswers){
+      for(var key in userAnswers[index]){
+    course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.testModel[index][key]=userAnswers[index][key];
+        }
+    }
+
+db.clnUserCourseMapping.save(course);
+
+return {success:true};
+}
+});
+
+
