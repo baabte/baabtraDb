77e8857193b884cb68d00cf00e1ecd42e27c4aef
Anoop <anoop@baabte.com>
2015-04-28 16:36:51 +0530

    added the assignment viewer and assignment evaluator

--- Functions/fnSaveUserAnswer.js
+++ Functions/fnSaveUserAnswer.js
@@ -6,46 +6,40 @@ Purpose : For submitting answer of a course
 */
 
 db.system.js.save({_id: "fnSaveUserAnswer",
-        value: function(courseId,userLoginId,keyName,tlPointInmins,outerIndex,innerIndex,answerObj) {
-    
-    var course=db.clnUserCourseMapping.findOne({fkCourseId:courseId,fkUserLoginId:userLoginId,activeFlag:1});
-    var currentMark=0;
-    
-    //checks if he have already scored marks
-    if(!course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored){
-    course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored=0;  
+        value: function (courseId, userLoginId, keyName, tlPointInmins, outerIndex, innerIndex, answerObj) {
+    var course = db.clnUserCourseMapping.findOne({fkCourseId:courseId, fkUserLoginId:userLoginId, activeFlag:1});
+    var currentMark = 0;
+    var evalStatus;
+    if (answerObj.evaluated == 0) {
+        evalStatus = "Pending Evaluation";
+    } else if (answerObj.evaluated == 1) {
+        evalStatus = "Evaluated";
+    } else if (answerObj.evaluated == 2) {
+        evalStatus = "Draft";
     }
-    //checks if he have already scored marks
-    if(!course.courseTimeline[tlPointInmins].markScored){
-    course.courseTimeline[tlPointInmins].markScored=0;  
+    course.courseTimeline[tlPointInmins][keyName][outerIndex].evalStatus = evalStatus;
+    if (!course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored) {
+        course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored = 0;
     }
-    //checks if he have already scored marks
-    if(!course.markScored){
-    course.markScored=0;  
+    if (!course.courseTimeline[tlPointInmins].markScored) {
+        course.courseTimeline[tlPointInmins].markScored = 0;
     }
-    
-    // copying current mark for calculations
-    currentMark=course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored
-    
-    //checks if he have already answered this question and if yes then deducts that mark from total mark
-    if(course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored){
-        course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored=currentMark-course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored;
-        currentMark=course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored;
-        course.courseTimeline[tlPointInmins].markScored=course.courseTimeline[tlPointInmins].markScored-course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored;
-        course.markScored=course.markScored-course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored;  
+    if (!course.markScored) {
+        course.markScored = 0;
     }
-    
-    //setting scored mark on question
-    for(key in answerObj){
-    
-    course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value[key]=answerObj[key];
-        
+    currentMark = course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored;
+    if (course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored) {
+        course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored = currentMark - course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored;
+        currentMark = course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored;
+        course.courseTimeline[tlPointInmins].markScored = course.courseTimeline[tlPointInmins].markScored - course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored;
+        course.markScored = course.markScored - course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value.markScored;
     }
-    
-    //adding scored mark to the course element
-    course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored=currentMark+answerObj.markScored;
-    course.courseTimeline[tlPointInmins].markScored=course.courseTimeline[tlPointInmins].markScored+answerObj.markScored;
-    course.markScored=course.markScored+answerObj.markScored;
-    
-db.clnUserCourseMapping.save(course);
-       return {success:true};}});
+    for (key in answerObj) {
+        course.courseTimeline[tlPointInmins][keyName][outerIndex].elements[innerIndex].value[key] = answerObj[key];
+    }
+    course.courseTimeline[tlPointInmins][keyName][outerIndex].markScored = currentMark + answerObj.markScored;
+    course.courseTimeline[tlPointInmins].markScored = course.courseTimeline[tlPointInmins].markScored + answerObj.markScored;
+    course.markScored = course.markScored + answerObj.markScored;
+    db.clnUserCourseMapping.save(course);
+    return {success:true};
+}});
