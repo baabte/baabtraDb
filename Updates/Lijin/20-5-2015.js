bd27f8fc472676788ec9c8efcf1ea7356c68d575
Lijin <lijin@baabte.com>
2015-05-20 18:46:59 +0530

    Added function for setting candidate age range

---
+++ Updates/Lijin/20-5-2015.js
@@ -0,0 +1,5 @@
+db.system.js.save({_id:'fnUpdateCandidateAgeLimit',
+	value:function(companyId,limitRange){
+   db.clnGlobalSettings.update({companyId:companyId},{$set:{candidateAgeLimit:limitRange}});
+   return db.clnGlobalSettings.findOne({companyId:companyId});
+}});
