c7bae37fc803255049b7f080838da48ef02a36ca
arunbaabte <arun@baabte.com>
2015-07-03 13:21:48 +0530

    commit with latest changes

--- Functions/fnLoadMenteesForPayment.js
+++ Functions/fnLoadMenteesForPayment.js
@@ -2,7 +2,7 @@ db.system.js.save({
 _id:"fnLoadMenteesForPayment",
 "value":function(companyId,pageNumber, nPerPage,searchKey,orderFormType){
 var data = {};
-
+    statusType=["Verified","Partially Paid"];
     if(orderFormType==''){
     data.type = "regex";
     data.orderFroms = db.clnTrainingRequest.find({
@@ -12,10 +12,8 @@ var data = {};
                 {customCompanyCode:{$regex:new RegExp(searchKey, "i")}},
                 {'requesteeDetails.type':{$regex:new RegExp(searchKey, "i")}},
                 {'orderDetails.userInfo.userCode':{$regex:new RegExp(searchKey, "i")}}
-            ],
-            companyId:ObjectId(companyId),
-            //'orderDetails.userInfo.status':{$in:statusType}
-            'orderDetails.userInfo.statusHistory.statusChangedTo':{$nin:["paid","Paid"]},
+            ],companyId:ObjectId(companyId),status:{$in:statusType},
+            'statusHistory.statusChangedTo':{$nin:["paid","Paid"]},
             }).skip(pageNumber > 0 ? (pageNumber - 1) * nPerPage : 0).sort({customCompanyCode:-1}).limit(nPerPage).toArray();
 
     }else{
@@ -27,11 +25,9 @@ var data = {};
                 {customCompanyCode:{$regex:new RegExp(searchKey, "i")}},
                 {'requesteeDetails.type':{$regex:new RegExp(searchKey, "i")}},
                 {'orderDetails.userInfo.userCode':{$regex:new RegExp(searchKey, "i")}}
-            ],
-            companyId:ObjectId(companyId),
-            'requesteeDetails.type':orderFormType,
-            //'orderDetails.userInfo.status':{$in:statusType}
-            'orderDetails.userInfo.statusHistory.statusChangedTo':{$nin:["paid","Paid"]},
+            ],companyId:ObjectId(companyId),
+            'requesteeDetails.type':orderFormType,status:{$in:statusType},
+            'statusHistory.statusChangedTo':{$nin:["paid","Paid"]},
             }).skip(pageNumber > 0 ? (pageNumber - 1) * nPerPage : 0).sort({customCompanyCode:-1}).limit(nPerPage).toArray();
 
 
